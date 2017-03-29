620f6cd172e5f3cf7ceaf7c407632976316f851d
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-29 12:34:03 -0400

    pass validation test for reviews

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+  @user = User.create!(username:"nancy",email:"nancy@email.com", password:"password")
+  @movie = Movie.create!(title:"The Fox and the Hound", year:1981, genre:"kids", cast:" Kurt Russell, Mickey Rooney", director:" Richard Rich, Ted Berman, Art Stevens")
+  @review = @user.reviews.create!(content:"So good",ratings:10,movie_id:@movie.id)
+  # @comment = @user.comments.create!(content:"You're wrong!",commentable_type:"Movie")
+  end
+  describe 'associations' do
+
+      it 'returns the reviews it has left ' do
+        expect(@user.reviews).to match_array [@review]
+      end
+
+      # it 'returns the comments it has left ' do
+      #   expect(@user.comments).to match_array [@comment]
+      # end
+
+  end
 end
