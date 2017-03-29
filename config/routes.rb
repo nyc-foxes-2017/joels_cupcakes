16a2b0a5ee246b714bbdbc0459ad5c98c094a455
Thomas Chang <Tomchang93@gmail.com>
2017-03-29 10:24:54 -0400

    added some associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to 'movies#index'
+  root 'movies#index'
   resources :users
   resources :movies do
+    resources :comments
     resources :reviews
   end
 
