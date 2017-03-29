ac803102b492f53b764e45270a21e14381e44ee3
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-29 15:21:53 -0400

    write tests for associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) = User.new{(username:"nancy",email:"nancy@email.com", password:"password")}
-  let(:movie) = Movie.new(title:"The Fox and the Hound", year:1981, genre:"kids", cast:" Kurt Russell, Mickey Rooney", director:" Richard Rich, Ted Berman, Art Stevens")
-  let(:reviews) = user.reviews.create!(content:"So good",ratings:10,movie_id:movie.id)
-  # before(:each) do
-  # @user = User.create!(username:"nancy",email:"nancy@email.com", password:"password")
-  # @movie = Movie.create!(title:"The Fox and the Hound", year:1981, genre:"kids", cast:" Kurt Russell, Mickey Rooney", director:" Richard Rich, Ted Berman, Art Stevens")
-  # @review = @user.reviews.create!(content:"So good",ratings:10,movie_id:@movie.id)
-  # @comment = @user.comments.create!(content:"You're wrong!",commentable_type:"Movie")
-  # end
+  let(:user) {User.new(username:"nancy",email:"nancy@email.com", password:"password")}
+  let(:movie) {Movie.new(title:"The Fox and the Hound", year:1981, genre:"kids", cast:" Kurt Russell, Mickey Rooney", director:" Richard Rich, Ted Berman, Art Stevens")}
+  let(:review) {user.reviews.new(content:"So good",ratings:10,movie_id:movie.id)}
+  let(:comment) {user.comments.new(content:"You're wrong!",commentable_type:"Movie", commentable_id:movie.id)}
+  let(:fav_movie) {user.favorite_movies.new(movie_id:movie.id)}
+  let(:watchlist_movie) {user.watchlists.new(movie_id:movie.id)}
+
   describe 'associations' do
 
       it 'returns the reviews it has left ' do
-        expect(@user.reviews).to match_array [@review]
+        expect(user.reviews).to match_array [review]
       end
 
-      # it 'returns the comments it has left ' do
-      #   expect(@user.comments).to match_array [@comment]
+      it 'returns the comments it has left ' do
+        expect(user.comments).to match_array [comment]
+      end
+
+
+      # it "returns user's favorite movies" do
+      #   expect(user.favorite_movies).to match_array [fav_movie]
       # end
+
+      it "returns user's favorite movies" do
+
+        expect(user.favorites).to match_array [fav_movie]
+      end
+
+      # it "returns user's watchlist movies" do
+      #   expect(user.watchlists).to match_array [watchlist_movie]
+      # end
+
+      it "returns user's watchlist movies" do
+        expect(user.movies_to_watch).to match_array [watchlist_movie]
+      end
+
 
   end
 end
