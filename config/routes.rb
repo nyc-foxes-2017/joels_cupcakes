eeeeb58bc5464d65d529f5f3722da990c37392c2
Thomas Chang <Tomchang93@gmail.com>
2017-03-29 10:08:14 -0400

    setup nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to 'movies#index'
   resources :users
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
+
 end
