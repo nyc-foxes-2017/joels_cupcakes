5732fad733473f96a9c63912ff6e209b3ca3a38b
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-29 16:48:43 -0400

    write and pass association tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,41 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) {User.new(username:"nancy",email:"nancy@email.com", password:"password")}
-  let(:movie) {Movie.new(title:"The Fox and the Hound", year:1981, genre:"kids", cast:" Kurt Russell, Mickey Rooney", director:" Richard Rich, Ted Berman, Art Stevens")}
-  let(:review) {user.reviews.new(content:"So good",ratings:10,movie_id:movie.id)}
-  let(:comment) {user.comments.new(content:"You're wrong!",commentable_type:"Movie", commentable_id:movie.id)}
-  let(:fav_movie) {user.favorite_movies.new(movie_id:movie.id)}
-  let(:watchlist_movie) {user.watchlists.new(movie_id:movie.id)}
+  # let(:user) {User.create!(username:"nancy",email:"nancy@email.com", password:"password")}
+  # let(:movie) {Movie.create!(title:"The Fox and the Hound", year:1981, genre:"kids", cast:" Kurt Russell, Mickey Rooney", director:" Richard Rich, Ted Berman, Art Stevens")}
+  # let(:review) {user.reviews.create!(content:"So good",ratings:10,movie_id:movie.id)}
+  # let(:comment) {user.comments.create!(content:"You're wrong!",commentable_type:"Movie", commentable_id:movie.id)}
+  # let(:fav_movie) {user.favorite_movies.create!(movie_id:movie.id)}
+  # let(:watchlist_movie) {user.watchlists.create!(movie_id:movie.id)}
 
   describe 'associations' do
+    before(:each) do
+      @user = User.create!(username:"nancy",email:"nancy@email.com", password:"password")
+      @movie = Movie.create!(title:"The Fox and the Hound")
+      @other_movie = Movie.create!(title:"Movie Title")
+      @review = @user.reviews.create!(content:"So good",ratings:10,movie_id:@movie.id)
+      @comment = @user.comments.create!(content:"You're wrong!",commentable_type:"Movie", commentable_id: @movie.id)
+      @fav_movie = @user.favorite_movies.create!(movie_id: @movie.id)
+
+      @watchlist_movie = @user.watchlists.create!(movie_id: @movie.id)
+    end
+
 
       it 'returns the reviews it has left ' do
-        expect(user.reviews).to match_array [review]
+        expect(@user.reviews).to match_array [@review]
       end
 
       it 'returns the comments it has left ' do
-        expect(user.comments).to match_array [comment]
+        expect(@user.comments).to match_array [@comment]
       end
 
 
       # it "returns user's favorite movies" do
-      #   expect(user.favorite_movies).to match_array [fav_movie]
+      #   expect(@user.favorite_movies).to match_array [@fav_movie]
       # end
+
+
+
+
 
       it "returns user's favorite movies" do
 
-        expect(user.favorites).to match_array [fav_movie]
+        expect(@user.favorites).to match_array [@movie]
       end
 
-      # it "returns user's watchlist movies" do
-      #   expect(user.watchlists).to match_array [watchlist_movie]
-      # end
+      it "does not return a movie that has not been favorited" do
+
+        expect(@user.favorites).not_to include(@other_movie)
+      end
+
 
       it "returns user's watchlist movies" do
-        expect(user.movies_to_watch).to match_array [watchlist_movie]
+
+        expect(@user.movies_to_watch).to match_array [@movie]
+      end
+
+      it "does not return a movie that has not been added to watchlist" do
+
+        expect(@user.movies_to_watch).not_to include(@other_movie)
       end
 
 
-  end
+end
+
+
+
 end
