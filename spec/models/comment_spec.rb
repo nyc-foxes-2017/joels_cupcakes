3e8719919547907961d59431b71d189b2dbddb77
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-29 18:04:07 -0400

    start writing tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(username:"nancy",email:"nancy@email.com", password:"password")
+    @movie = Movie.create!(title:"The Fox and the Hound")
+    @comment = @user.comments.create!(content:"Movie rule!", commentable_type:'Movie', commentable_id:@movie.id)
+    @comment_on_comment = @comment.create!(comment:"No they don't!")
+  end
+
+      it 'returns the user who left the comment' do
+        expect(@comment.users).to match_array [@user]
+      end
 end
