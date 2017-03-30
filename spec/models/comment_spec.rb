ccdcbc9741c6cf3065f962b7872abd25ce3bc6b3
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-30 10:02:02 -0400

    write and pass tests for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
+
 RSpec.describe Comment, type: :model do
   before(:each) do
     @user = User.create!(username:"nancy",email:"nancy@email.com", password:"password")
     @movie = Movie.create!(title:"The Fox and the Hound")
     @comment = @user.comments.create!(content:"Movie rule!", commentable_type:'Movie', commentable_id:@movie.id)
-    @comment_on_comment = @comment.create!(comment:"No they don't!")
+    @comment_on_comment = @comment.comments.create!(content:"No they don't!", commentable_type:'Comment',commentable_id:@comment.id, user_id:@user.id)
   end
 
-      it 'returns the user who left the comment' do
-        expect(@comment.users).to match_array [@user]
+      it 'returns the username who left the comment' do
+        expect(@comment.user.username).to eq(@user.username)
       end
+
+      it 'returns the username who left the comment on a comment' do
+        expect(@comment_on_comment.user.username).to eq(@user.username)
+      end
+
+      it 'returns the comments of a certain comment' do
+        expect(@comment.comments).to match_array [@comment_on_comment]
+      end
+
+
+
 end
