2590b02e7fbabfc7de64fbf6cf1c310e57ef6d85
ThomasChang7 <email": null>
2017-03-30 18:52:07 -0400

    added helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,8 @@ module UsersHelper
   def check_watchlist(movie)
     current_user.movies_to_watch.include?(movie)
   end
+
+  def reviewed?(movie)
+    movie.reviews.any? {|review| review.user_id == current_user.id }
+  end
 end
