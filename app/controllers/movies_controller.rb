677249e2e8bc4d5dc5ecdef8656a71f69ffa3eec
ThomasChang7 <email": null>
2017-03-30 12:02:52 -0400

    implemented favorite and watchlist features

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
 
   def index
+    
     @movies = Movie.all
+    @most_recent = Movie.all.sample(4)
   end
 
   def show
@@ -17,11 +19,24 @@ class MoviesController < ApplicationController
         movie = JSON.parse open("http://www.omdbapi.com/?t=" + string).read
         @movie = Movie.create!( { title: movie["Title"]} )
         @movies = Movie.search(params[:search]).order("created_at DESC")
+        byebug
       end
     else
       @movies = Movie.all.order("created_at DESC")
       redirect_to '/'
     end
+  end
+
+  def favorite
+    movie = Movie.find(params[:id])
+    current_user.favorites.include?(movie) ? current_user.favorites.delete(movie) : current_user.favorites << movie 
+    redirect_to movie_path(movie)
+  end
+
+  def watchlist
+    movie = Movie.find(params[:id])
+    current_user.movies_to_watch.include?(movie) ? current_user.movies_to_watch.delete(movie) : current_user.movies_to_watch << movie 
+    redirect_to movie_path(movie)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/search', :to => 'movies#search'
+  post '/movies/:id/favorite', :to => 'movies#favorite'
+  post '/movies/:id/watchlist', :to => 'movies#watchlist'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
