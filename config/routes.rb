d3b2c17134dd4438cceb88025fb44e1f9558c197
Joel Lebron <email": null>
2017-03-30 11:45:30 -0400

    Devise user registration + login/logout functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get '/search', :to => 'movies#search'
-  devise_for :users
+  devise_for :users, :path_names => { :sign_up => "register" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
   resources :users
